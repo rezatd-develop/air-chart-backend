bdf27be306eac527dacfdd104cbd9b57a757f042
rezatd-develop <reza.td.develop@gmail.com>
2025-10-07 12:17:17 +0330

    release v 0.5 (beta)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import userRoutes from "./routes/userRoutes.js";
 import filesRoutes from './routes/filesRoutes/filesRoutes.js';
 import dashboardRoutes from './routes/dashboard/dashboardRoutes.js';
 import authRoutes from "./routes/authRoutes/authRoutes.js";
@@ -10,7 +9,7 @@ import { verifyToken } from "./middlewares/authMiddleware.js";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: "http://localhost:4000", 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
@@ -21,7 +20,6 @@ connectDB();
 
 app.use(verifyToken);
 
-app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/files", filesRoutes);
 app.use("/api/dashboard", dashboardRoutes);
--- src/controllers/authController/authController.js
+++ src/controllers/authController/authController.js
@@ -35,16 +35,32 @@ export const signIn = async (req, res) => {
         const { username, password } = req.body;
 
         const user = await User.findOne({ username });
-        if (!user) return res.status(200).json(createResponseMessageClass(null, true, translations.userNotFound));
+        if (!user)
+            return res.status(200).json(
+                createResponseMessageClass(null, true, translations.userNotFound)
+            );
 
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
-        if (!isPasswordCorrect) return res.status(401).json(createResponseMessageClass(null, true, translations.invalidCredentials));
+        if (!isPasswordCorrect)
+            return res.status(401).json(
+                createResponseMessageClass(null, true, translations.invalidCredentials)
+            );
 
         const token = generateToken(user);
+        const userFullName = `${user.firstName} ${user.lastName}`;
 
-        res.json(createResponseMessageClass(token, false, translations.loginSuccessful));
+        res.json(
+            createResponseMessageClass(
+                { token, userFullName },
+                false,
+                translations.loginSuccessful
+            )
+        );
     } catch (error) {
         console.error(error);
-        res.status(500).json(createResponseMessageClass(null, true, translations.signInFailed));
+        res.status(500).json(
+            createResponseMessageClass(null, true, translations.signInFailed)
+        );
     }
 };
+
